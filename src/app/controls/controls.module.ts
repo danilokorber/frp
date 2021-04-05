e1f1f514771989c0af096c4134b55284f20cd949
Danilo Koerber (FG-810) <danilo@easyware.com.br>
2021-04-05 22:01:00 +0200

    Reset

--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LeftMenuItemControl } from './layout/left-menu-item.component';
 import { MailAlertControl } from './layout/mail-alert.component';
 import { ZorroModule } from '@modules/zorro/zorro.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NotificationAlertControl } from './layout/notification-alert.component';
 
 @NgModule({
-  declarations: [LeftMenuItemControl, MailAlertControl, NotificationAlertControl],
+  declarations: [MailAlertControl, NotificationAlertControl],
   imports: [CommonModule, FontAwesomeModule, ZorroModule],
-  exports: [LeftMenuItemControl, MailAlertControl, NotificationAlertControl],
+  exports: [MailAlertControl, NotificationAlertControl],
 })
 export class ControlsModule {}
