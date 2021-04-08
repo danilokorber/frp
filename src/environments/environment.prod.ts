aaa3546ec56b36e77c35da3c3ffb7ed2df427d2f
Danilo Koerber (FG-810) <danilo@easyware.com.br>
2021-04-08 10:55:45 +0200

    Fixed environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,15 @@ import { templateVersion } from '../../package.json';
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 const authConfig: AuthConfig = {
+  clientId: 'frontend',
+  disableAtHashCheck: true,
   issuer: 'https://auth.easyware.io/auth/realms/Platypus',
+  oidc: true,
   redirectUri: window.location.origin + '/Home',
-  clientId: 'account',
   responseType: 'code',
-  scope: 'openid profile email offline_access api',
-  showDebugInformation: false,
+  scope: 'profile email offline_access',
+  sessionChecksEnabled: true,
+  showDebugInformation: true,
 };
 
 export const environment = {
